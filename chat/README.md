2993cbdfbac73fce8e930d23872e0f24887a544d
Thomas Marangoni <thomas.marangoni@hotmail.com>
2020-11-29 17:19:42 +0100

    added chat

---
+++ chat/README.md
@@ -0,0 +1,47 @@
+# alt:V Chat
+
+You can start by adding the chat resource in its own folder called 'chat'.
+```
+altVServerFolder/
+└── resources/
+    ├── chat/
+    |   ├── index.mjs
+    |   ├── client.mjs
+    |   ├── resource.cfg
+    |   └── html/
+    └── your_resource/
+        ├── your_resource_main.mjs
+        ├── your_resource_client.mjs
+        └── your_resource.cfg
+```
+
+**This is for YOUR resource that you want to implement the chat resource into.**
+resource.cfg
+```
+type: js,
+main: your_resource_main.mjs
+client-main: your_resource_client.mjs
+client-files: [],
+deps: [
+    chat
+]
+```
+
+### General Usage
+
+**Serverside**
+```
+import * as chat from 'chat';
+
+// Uses the chat resource to register a command.
+// Sends a chat message to the player with their position information.
+chat.registerCmd('pos', (player, args) => {
+    chat.send(player, `X: ${player.pos.x}, Y: ${player.pos.y}, Z: ${player.pos.z}`);
+    
+    // Sends to all players.
+    chat.broadcast(`${player.name} is located at: ${player.pos.x}, Y: ${player.pos.y}, Z: ${player.pos.z}`);
+});
+```
+
+
+
---
+++ chat/server.js
@@ -0,0 +1,87 @@
+import alt from 'alt';
+
+let cmdHandlers = {};
+let mutedPlayers = new Map();
+
+function invokeCmd(player, cmd, args) {
+  cmd = cmd.toLowerCase();
+  const callback = cmdHandlers[cmd];
+
+  if (callback) {
+    callback(player, args);
+  } else {
+    send(player, `{FF0000} Unknown command /${cmd}`);
+  }
+}
+
+alt.onClient('chat:message', (player, msg) => {
+  if (msg[0] === '/') {
+    msg = msg.trim().slice(1);
+
+    if (msg.length > 0) {
+      alt.log('[chat:cmd] ' + player.name + ': /' + msg);
+
+      let args = msg.split(' ');
+      let cmd = args.shift();
+
+      invokeCmd(player, cmd, args);
+    }
+  } else {
+    if (mutedPlayers.has(player) && mutedPlayers[player]) {
+      send(player, '{FF0000} You are currently muted.');
+      return;
+    }
+
+    msg = msg.trim();
+
+    if (msg.length > 0) {
+      alt.log('[chat:msg] ' + player.name + ': ' + msg);
+
+      alt.emitClient(null, 'chat:message', player.name, msg.replace(/</g, '&lt;').replace(/'/g, '&#39').replace(/"/g, '&#34'));
+    }
+  }
+});
+
+export function send(player, msg) {
+  alt.emitClient(player, 'chat:message', null, msg);
+}
+
+export function broadcast(msg) {
+  send(null, msg);
+}
+
+export function registerCmd(cmd, callback) {
+  cmd = cmd.toLowerCase();
+
+  if (cmdHandlers[cmd] !== undefined) {
+    alt.logError(`Failed to register command /${cmd}, already registered`);
+  } else {
+    cmdHandlers[cmd] = callback;
+  }
+}
+
+export function mutePlayer(player, state) {
+  mutedPlayers.set(player, state);
+}
+
+// Used in an onConnect function to add functions to the player entity for a seperate resource.
+export function setupPlayer(player) {
+  player.sendMessage = (msg) => {
+    send(player, msg);
+  }
+
+  player.mute = (state) => {
+    mutePlayer(player, state);
+  }
+}
+
+// Arbitrary events to call.
+alt.on('sendChatMessage', (player, msg) => {
+  alt.logWarning('Usage of chat events is deprecated use export functions instead');
+  send(player, msg);
+});
+
+alt.on('broadcastMessage', (msg) => {
+  alt.logWarning('Usage of chat events is deprecated use export functions instead');
+  send(null, msg);
+});
