57b6d1ef4a2c764f68a624679c203df1893ee83c
Blue <florian.uaz@web.de>
2021-06-27 20:30:43 +0200

    Fix focus issues after altv update

--- chat/client.js
+++ chat/client.js
@@ -1,4 +1,4 @@
-import * as alt from 'alt';
+import * as alt from 'alt-client';
 
 let buffer = [];
 
@@ -8,58 +8,62 @@ let opened = false;
 const view = new alt.WebView("http://resource/html/index.html");
 
 function addMessage(name, text) {
-  if (name) {
-    view.emit('addMessage', name, text);
-  } else {
-    view.emit('addString', text);
-  }
+    if (name) {
+        view.emit('addMessage', name, text);
+    } else {
+        view.emit('addString', text);
+    }
 }
 
 view.on('chatloaded', () => {
-  for (const msg of buffer) {
-    addMessage(msg.name, msg.text);
-  }
+    for (const msg of buffer) {
+        addMessage(msg.name, msg.text);
+    }
 
-  loaded = true;
+    loaded = true;
 })
 
 view.on('chatmessage', (text) => {
-  alt.emitServer('chat:message', text);
+    alt.emitServer('chat:message', text);
 
-  opened = false;
-  alt.toggleGameControls(true);
+    opened = false;
+    alt.toggleGameControls(true);
+    view.unfocus();
 })
 
 export function pushMessage(name, text) {
-  if (!loaded) {
-    buffer.push({ name, text });
-  } else {
-    addMessage(name, text);
-  }
+    if (!loaded) {
+        buffer.push({ name, text });
+    } else {
+        addMessage(name, text);
+    }
 }
 
 export function pushLine(text) {
-  pushMessage(null, text);
+    pushMessage(null, text);
 }
 
 alt.onServer('chat:message', pushMessage);
 
 alt.on('keyup', (key) => {
-  if (loaded) {
-    if (!opened && key === 0x54 && alt.gameControlsEnabled()) {
-      opened = true;
-      view.emit('openChat', false);
-      alt.toggleGameControls(false);
-    } else if (!opened && key === 0xBF && alt.gameControlsEnabled()) {
-      opened = true;
-      view.emit('openChat', true);
-      alt.toggleGameControls(false);
-    } else if (opened && key == 0x1B) {
-      opened = false;
-      view.emit('closeChat');
-      alt.toggleGameControls(true);
+    if (loaded) {
+        if (!opened && key === 0x54 && alt.gameControlsEnabled()) {
+            opened = true;
+            view.emit('openChat', false);
+            alt.toggleGameControls(false);
+            view.focus();
+        } else if (!opened && key === 0xBF && alt.gameControlsEnabled()) {
+            opened = true;
+            view.emit('openChat', true);
+            alt.toggleGameControls(false);
+            view.focus();
+        } else if (opened && key == 0x1B) {
+            opened = false;
+            view.emit('closeChat');
+            alt.toggleGameControls(true);
+            view.unfocus();
+        }
     }
-  }
 });
 
 pushLine('<b>alt:V Multiplayer has started</b>');
