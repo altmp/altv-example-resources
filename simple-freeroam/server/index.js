d706909cb0aa6f885ca44d35232efc9b33978d39
tastydev <39250826+tastydev@users.noreply.github.com>
2021-06-29 00:26:11 +0200

    refactor(freeroam): Update to latest API changes (#7)

--- simple-freeroam/server/index.js
+++ simple-freeroam/server/index.js
@@ -1,4 +1,4 @@
-import * as alt from 'alt';
+import * as alt from 'alt-server';
 import * as chat from 'chat';
 
 const spawns = [
@@ -17,6 +17,97 @@ const spawnModels = [
     "mp_m_weapexp_01"
 ];
 
+const weapons = [
+    "dagger",
+    "bat",
+    "bottle",
+    "crowbar",
+    "flashlight",
+    "golfclub",
+    "hammer",
+    "hatchet",
+    "knuckle",
+    "knife",
+    "machete",
+    "switchblade",
+    "nightstick",
+    "wrench",
+    "battleaxe",
+    "poolcue",
+    "stone_hatchet",
+    "pistol",
+    "pistol_mk2",
+    "combatpistol",
+    "appistol",
+    "stungun",
+    "pistol50",
+    "snspistol",
+    "snspistol_mk2",
+    "heavypistol",
+    "vintagepistol",
+    "flaregun",
+    "marksmanpistol",
+    "revolver",
+    "revolver_mk2",
+    "doubleaction",
+    "raypistol",
+    "microsmg",
+    "smg",
+    "smg_mk2",
+    "assaultsmg",
+    "combatpdw",
+    "machinepistol",
+    "minismg",
+    "raycarbine",
+    "pumpshotgun",
+    "pumpshotgun_mk2",
+    "sawnoffshotgun",
+    "assaultshotgun",
+    "bullpupshotgun",
+    "musket",
+    "heavyshotgun",
+    "dbshotgun",
+    "autoshotgun",
+    "assaultrifle",
+    "assaultrifle_mk2",
+    "carbinerifle",
+    "carbinerifle_mk2",
+    "advancedrifle",
+    "specialcarbine",
+    "specialcarbine_mk2",
+    "bullpuprifle",
+    "bullpuprifle_mk2",
+    "compactrifle",
+    "mg",
+    "combatmg",
+    "combatmg_mk2",
+    "gusenberg",
+    "sniperrifle",
+    "heavysniper",
+    "heavysniper_mk2",
+    "marksmanrifle",
+    "marksmanrifle_mk2",
+    "rpg",
+    "grenadelauncher",
+    "grenadelauncher_smoke",
+    "minigun",
+    "firework",
+    "railgun",
+    "hominglauncher",
+    "compactlauncher",
+    "rayminigun",
+    "grenade",
+    "bzgas",
+    "smokegrenade",
+    "flare",
+    "molotov",
+    "stickybomb",
+    "proxmine",
+    "snowball",
+    "pipebomb",
+    "ball"
+];
+
 function randomNumber(min, max) {  
     return Math.round(Math.random() * (max - min) + min); 
 }
@@ -25,7 +116,7 @@ function getRandomListEntry(list){
     return randomNumber(0, list.length - 1);
 }
 
-alt.on('playerConnect', function (player) {
+alt.on('playerConnect', (player) => {
     if(player.name.includes("admin")){
         player.kick();
         return;
@@ -33,45 +124,51 @@ alt.on('playerConnect', function (player) {
 
     player.model = spawnModels[getRandomListEntry(spawnModels)];
     player.setMeta("vehicles", []);
-    var spawn = spawns[getRandomListEntry(spawns)];
+    const spawn = spawns[getRandomListEntry(spawns)];
     player.spawn(spawn.x, spawn.y, spawn.z, 0);
     alt.emitClient(player, "freeroam:spawned");
     alt.emitClient(player, "freeroam:Interiors");
 
-    setTimeout(function(){ 
-        if(player !== undefined){
-            let playerCount = alt.Player.all.length;
+    let connectTimeout = alt.setTimeout(() => { 
+        if(player && player.valid){
+            const playerCount = alt.Player.all.length;
             chat.broadcast(`{1cacd4}${player.name} {ffffff}has {00ff00}joined {ffffff}the Server..  (${playerCount} players online)`);
             chat.send(player, "{80eb34}Press {34dfeb}T {80eb34}and type {34dfeb}/help {80eb34}to see all available commands..");
         }
+        alt.clearTimeout(connectTimeout);
     }, 1000);
 });
 
 alt.on('playerDeath', (player, killer, weapon) => {
-    var spawn = spawns[randomNumber(0, spawns.length - 1)];
+    const spawn = spawns[randomNumber(0, spawns.length - 1)];
     alt.emitClient(player, "freeroam:switchInOutPlayer", false, 0, 2);
-    setTimeout(function(){
-        if(player !== undefined){
+    let playerDeathTimeout = alt.setTimeout(() => {
+        if(player && player.valid){
             player.spawn(spawn.x, spawn.y, spawn.z, 0);
             alt.emitClient(player, "freeroam:switchInOutPlayer", true);
-            alt.emitClient(player, "freeroam:clearPedBloodDamage");
+            player.clearBloodDamage();
         }
+        alt.clearTimeout(playerDeathTimeout);
     }, 3000);
-    alt.log(`${killer.name} gave ${player.name} the rest!`);
-
-    SendNotificationToAllPlayer(`~r~<C>${killer.name}</C> ~s~killed ~b~<C>${player.name}</C>`);
+    if(killer){
+        alt.log(`${killer.name} gave ${player.name} the rest!`);
+        SendNotificationToAllPlayer(`~r~<C>${killer.name}</C> ~s~killed ~b~<C>${player.name}</C>`);
+    }else{
+        alt.log(`${player.name} died!`);
+        SendNotificationToAllPlayer(`~s~Suicide ~b~<C>${player.name}</C>`);
+    }
 });
 
-function SendNotificationToPlayer(player, message, textColor=0, bgColor=2, blink=false){
+function SendNotificationToPlayer(player, message, textColor=0, bgColor=2, blink=false) {
     alt.emitClient(player, "freeroam:sendNotification", textColor, bgColor, message, blink);
 }
 
-function SendNotificationToAllPlayer(message, textColor=0, bgColor=2, blink=false){
-    alt.Player.all.forEach(plr => SendNotificationToPlayer(plr, message, textColor, bgColor, blink));
+function SendNotificationToAllPlayer(message, textColor=0, bgColor=2, blink=false) {
+    alt.emitAllClients("freeroam:sendNotification", textColor, bgColor, message, blink);
 }
 
 alt.on('playerDisconnect', (player, reason) => {
-    let playerCount = alt.Player.all.length;
+    const playerCount = alt.Player.all.length;
     chat.broadcast(`{1cacd4}${player.name} {ffffff}has {ff0000}left {ffffff}the Server.. (${playerCount} players online)`);
     player.getMeta("vehicles").forEach(vehicle => {
         if(vehicle != null){
@@ -84,7 +181,7 @@ alt.on('playerDisconnect', (player, reason) => {
 
 // =============================== Commands Begin ==================================================
 
-chat.registerCmd("help", function (player, args) {
+chat.registerCmd("help", (player, args) => {
     chat.send(player, "{ff0000}========== {eb4034}HELP {ff0000} ==========");
     chat.send(player, "{ff0000}= {34abeb}/veh {40eb34}(model)   {ffffff} Spawn a Vehicle");
     chat.send(player, "{ff0000}= {34abeb}/tp {40eb34}(targetPlayer)   {ffffff} Teleport to Player");
@@ -94,47 +191,48 @@ chat.registerCmd("help", function (player, args) {
     chat.send(player, "{ff0000} ========================");
 });
 
-chat.registerCmd("veh", function (player, args) {
+chat.registerCmd("veh", (player, args) => {
     if (args.length === 0) {
         chat.send(player, "Usage: /veh (vehicleModel)");
         return;
     }
     try {
-        var vehicle = new alt.Vehicle(args[0], player.pos.x, player.pos.y, player.pos.z, 0, 0, 0);
-    }catch{
-        chat.send(player, `{ff0000} Vehicle Model {ff9500}${args[0]} {ff0000}does not exist..`);
-    }finally {
-        var pvehs = player.getMeta("vehicles");
+        let vehicle = new alt.Vehicle(args[0], player.pos.x, player.pos.y, player.pos.z, 0, 0, 0);
+        let pvehs = player.getMeta("vehicles");
         if(pvehs.length >= 3){
-            var toDestroy = pvehs.pop();
+            let toDestroy = pvehs.pop();
             if(toDestroy != null){
                 toDestroy.destroy();
             }
         }
         pvehs.unshift(vehicle);
         player.setMeta("vehicles", pvehs);
+    }catch(e) {
+        chat.send(player, `{ff0000} Vehicle Model {ff9500}${args[0]} {ff0000}does not exist..`);
+        alt.log(e);
     }
 });
 
-chat.registerCmd("pos", function (player, args) {
+chat.registerCmd("pos", (player, args) => {
     alt.log(`Position: ${player.pos.x}, ${player.pos.y}, ${player.pos.z}`);
     chat.send(player, `Position: ${player.pos.x}, ${player.pos.y}, ${player.pos.z}`);
 });
 
-chat.registerCmd("tp", function (player, args) {
-    if (args.length === 0) {
+chat.registerCmd("tp", (player, args) => {
+    if (args && args.length === 0) {
         chat.send(player, "Usage: /tp (target player)");
         return;
     }
-    let players = alt.getPlayersByName(args.join(' '));
-    if(players.length === 0){
-        chat.send(player, `{ff0000} Player {ff9500}${args.join(' ')} {ff0000}not found..`);
-    }else{
-        player.pos = players[0].pos;
+    const foundPlayers = alt.Player.all.filter(p => p.name === args[0]);
+    if(foundPlayers && foundPlayers.length > 0) {
+        player.pos = foundPlayers[0].pos;
+        chat.send(player, `You got teleported to {1cacd4}${foundPlayers[0].name}{ffffff}`);
+    } else {
+        chat.send(player, `{ff0000} Player {ff9500}${args[0]} {ff0000}not found..`);
     }
 });
 
-chat.registerCmd("model", function (player, args) {
+chat.registerCmd("model", (player, args) => {
     if (args.length === 0) {
         chat.send(player, "Usage: /model (modelName)");
         return;
@@ -142,7 +240,7 @@ chat.registerCmd("model", function (player, args) {
     player.model = args[0];
 });
 
-chat.registerCmd("weapon", function (player, args) {
+chat.registerCmd("weapon", (player, args) => {
     if (args.length === 0) {
         chat.send(player, "Usage: /weapon (modelName)");
         return;
@@ -150,100 +248,10 @@ chat.registerCmd("weapon", function (player, args) {
     player.giveWeapon(alt.hash("weapon_" + args[0]), 500, true);
 });
 
-chat.registerCmd("weapons", function (player, args) {
-    var weapons = [
-        "dagger",
-        "bat",
-        "bottle",
-        "crowbar",
-        "flashlight",
-        "golfclub",
-        "hammer",
-        "hatchet",
-        "knuckle",
-        "knife",
-        "machete",
-        "switchblade",
-        "nightstick",
-        "wrench",
-        "battleaxe",
-        "poolcue",
-        "stone_hatchet",
-        "pistol",
-        "pistol_mk2",
-        "combatpistol",
-        "appistol",
-        "stungun",
-        "pistol50",
-        "snspistol",
-        "snspistol_mk2",
-        "heavypistol",
-        "vintagepistol",
-        "flaregun",
-        "marksmanpistol",
-        "revolver",
-        "revolver_mk2",
-        "doubleaction",
-        "raypistol",
-        "microsmg",
-        "smg",
-        "smg_mk2",
-        "assaultsmg",
-        "combatpdw",
-        "machinepistol",
-        "minismg",
-        "raycarbine",
-        "pumpshotgun",
-        "pumpshotgun_mk2",
-        "sawnoffshotgun",
-        "assaultshotgun",
-        "bullpupshotgun",
-        "musket",
-        "heavyshotgun",
-        "dbshotgun",
-        "autoshotgun",
-        "assaultrifle",
-        "assaultrifle_mk2",
-        "carbinerifle",
-        "carbinerifle_mk2",
-        "advancedrifle",
-        "specialcarbine",
-        "specialcarbine_mk2",
-        "bullpuprifle",
-        "bullpuprifle_mk2",
-        "compactrifle",
-        "mg",
-        "combatmg",
-        "combatmg_mk2",
-        "gusenberg",
-        "sniperrifle",
-        "heavysniper",
-        "heavysniper_mk2",
-        "marksmanrifle",
-        "marksmanrifle_mk2",
-        "rpg",
-        "grenadelauncher",
-        "grenadelauncher_smoke",
-        "minigun",
-        "firework",
-        "railgun",
-        "hominglauncher",
-        "compactlauncher",
-        "rayminigun",
-        "grenade",
-        "bzgas",
-        "smokegrenade",
-        "flare",
-        "molotov",
-        "stickybomb",
-        "proxmine",
-        "snowball",
-        "pipebomb",
-        "ball"
-    ];
-    weapons.forEach(element => {
-        player.giveWeapon(alt.hash("weapon_" + element), 500, true);
-    });
+chat.registerCmd("weapons", (player, args) => {
+    for(let weapon of weapons){
+        player.giveWeapon(alt.hash("weapon_" + weapon), 500, true);
+    }
 });
 
 // =============================== Commands End ====================================================
