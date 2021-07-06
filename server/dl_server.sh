c414879cbed2f372aed6c295848b68cbaf4b16d9
Arochka <admin@arochka.com>
2021-07-06 13:56:41 +0200

    Produice release/rc/dev images

---
+++ server/dl_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget --no-cache -q -O ./altv-server https://cdn.altv.mp/server/$1/x64_linux/altv-server
+wget --no-cache -q -O ./modules/js-module/libnode.so.72 https://cdn.altv.mp/js-module/$1/x64_linux/modules/js-module/libnode.so.72
+wget --no-cache -q -O ./modules/js-module/libjs-module.so https://cdn.altv.mp/js-module/$1/x64_linux/modules/js-module/libjs-module.so
+wget --no-cache -q -O ./modules/libcsharp-module.so https://cdn.altv.mp/coreclr-module/$1/x64_linux/modules/libcsharp-module.so
+wget --no-cache -q -O ./AltV.Net.Host.dll https://cdn.altv.mp/coreclr-module/$1/x64_linux/AltV.Net.Host.dll
+wget --no-cache -q -O ./AltV.Net.Host.runtimeconfig.json https://cdn.altv.mp/coreclr-module/$1/x64_linux/AltV.Net.Host.runtimeconfig.json
