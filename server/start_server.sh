2da8ff3662b77523f02152692880713f3d230fb4
Arochka <admin@arochka.com>
2021-07-06 15:42:09 +0200

    Handle extraFolders

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+export extraFolders=""
+	
+for folder in resources-*
+do
+	export extraFolders="${extraFolders} --extra-res-folder \"${folder}\"" 
+done
+	
 if [[ ! -z "$CDN_PATH" ]]
 then
-	./altv-server --config "config/server.cfg" --host $ALT_HOST --port $ALT_PORT --justpack
+	./altv-server --config "config/server.cfg" $extraFolders --host $ALT_HOST --port $ALT_PORT --justpack
 	rm -r $CDN_PATH/*
 	mv ./cdn_upload/* $CDN_PATH/
 fi
 
 export CSHARP_MODULE_DISABLE_COLLECTIBLE=true
-./altv-server --config "config/server.cfg" --logfile "logs/server.log"
+./altv-server --config "config/server.cfg" --logfile "logs/server.log" $extraFolders
