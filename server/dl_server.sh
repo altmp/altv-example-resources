2a39d00a99bed23c6f3d15717b1ff52d4bfea3d9
Arochka <admin@arochka.com>
2021-07-07 15:09:53 +0200

    Fix libnode naming

--- server/dl_server.sh
+++ server/dl_server.sh
@@ -10,7 +10,7 @@ wget --no-cache -q -O ./data/vehmods.bin http://cdn.altv.mp/server/$1/x64_linux/
 
 if [ $1 != "release" ]
 then
-	wget --no-cache -q -O ./modules/js-module/libnode.so.72 https://cdn.altv.mp/js-module/$1/x64_linux/modules/js-module/libnode.so.83
+	wget --no-cache -q -O ./modules/js-module/libnode.so.83 https://cdn.altv.mp/js-module/$1/x64_linux/modules/js-module/libnode.so.83
 	wget --no-cache -q -O ./data/clothes.bin http://cdn.altv.mp/server/$1/x64_linux/data/clothes.bin
 else
 	wget --no-cache -q -O ./modules/js-module/libnode.so.72 https://cdn.altv.mp/js-module/$1/x64_linux/modules/js-module/libnode.so.72
