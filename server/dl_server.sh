7ddf9457de743a999121091e6940ce6bc8a9a885
Arochka <34453212+Arochka@users.noreply.github.com>
2021-08-07 12:41:31 +0200

    Update dl_server.sh

--- server/dl_server.sh
+++ server/dl_server.sh
@@ -8,10 +8,5 @@ wget --no-cache -q -O ./AltV.Net.Host.runtimeconfig.json https://cdn.altv.mp/cor
 wget --no-cache -q -O ./data/vehmodels.bin http://cdn.altv.mp/server/$1/x64_linux/data/vehmodels.bin
 wget --no-cache -q -O ./data/vehmods.bin http://cdn.altv.mp/server/$1/x64_linux/data/vehmods.bin
 
-if [ $1 != "release" ]
-then
-	wget --no-cache -q -O ./modules/js-module/libnode.so.83 https://cdn.altv.mp/js-module/$1/x64_linux/modules/js-module/libnode.so.83
-	wget --no-cache -q -O ./data/clothes.bin http://cdn.altv.mp/server/$1/x64_linux/data/clothes.bin
-else
-	wget --no-cache -q -O ./modules/js-module/libnode.so.72 https://cdn.altv.mp/js-module/$1/x64_linux/modules/js-module/libnode.so.72
-fi
+wget --no-cache -q -O ./modules/js-module/libnode.so.83 https://cdn.altv.mp/js-module/$1/x64_linux/modules/js-module/libnode.so.83
+wget --no-cache -q -O ./data/clothes.bin http://cdn.altv.mp/server/$1/x64_linux/data/clothes.bin
