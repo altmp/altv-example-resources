b4586e53baa957209b0e6bc515b878743bc66d06
Tim <wuestenhagen.tim@gmail.com>
2021-11-21 16:31:53 +0100

    add Player and Vehicle Factory

---
+++ freeroam-extended/server/freeroam-extended/Factories/PlayerFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using AltV.Net;
+using AltV.Net.Elements.Entities;
+
+namespace Freeroam_Extended.Factories
+{
+    public class AltPlayer : Player
+    {
+        public IList<AltVehicle> Vehicles { get; set; }
+        public DateTime LastVehicleSpawn { get; set; }
+
+        public AltPlayer(IServer server, IntPtr nativePointer, ushort id) : base(server, nativePointer, id)
+        {
+            Vehicles = new List<AltVehicle>();
+        }
+    }
+    
+    public class AltPlayerFactory : IEntityFactory<IPlayer>
+    {
+        public IPlayer Create(IServer server, IntPtr playerPointer, ushort id)
+        {
+            return new AltPlayer(server, playerPointer, id);
+        }
+    }
+}
---
+++ freeroam-extended/server/freeroam-extended/Factories/VehicleFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AltV.Net;
+using AltV.Net.Data;
+using AltV.Net.Elements.Entities;
+
+namespace Freeroam_Extended.Factories
+{
+    public class AltVehicle : Vehicle
+    {
+        public AltPlayer Owner { get; set; }
+        public DateTime SpawnTime { get; init; }
+        
+        public AltVehicle(IServer server, uint model, Position position, Rotation rotation) : base(server, model, position, rotation)
+        {
+            SpawnTime = DateTime.Now;
+        }
+        
+        public AltVehicle(IServer server, IntPtr nativePointer, ushort id) : base(server, nativePointer, id)
+        {
+            SpawnTime = DateTime.Now;
+        }
+    }
+    
+    public class AltVehicleFactory : IEntityFactory<IVehicle>
+    {
+        public IVehicle Create(IServer server, IntPtr playerPointer, ushort id)
+        {
+            return new AltVehicle(server, playerPointer, id);
+        }
+    }
+}
