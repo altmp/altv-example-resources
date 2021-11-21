fd8f9300d17a84eacf63037c5fd806615366baa6
Tim <wuestenhagen.tim@gmail.com>
2021-11-21 14:28:15 +0100

    init freeroam-extended project

---
+++ freeroam-extended/server/freeroam-extended/Main.cs
@@ -0,0 +1,17 @@
+﻿using AltV.Net;
+
+namespace Freeroam_Extended
+{
+    public class Main : Resource
+    {
+        public override void OnStart()
+        {
+            Alt.Server.LogColored("~g~ Freeroam-Extended Started!");
+        }
+
+        public override void OnStop()
+        {
+            Alt.Server.LogColored("~g~ Freeroam-Extended Stopped!");
+        }
+    }
+}
