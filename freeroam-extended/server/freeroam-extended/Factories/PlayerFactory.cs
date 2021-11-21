65d0075b80cd6ae4d2942a7284f1bb9f8b5603b7
Tim <wuestenhagen.tim@gmail.com>
2021-11-21 17:07:02 +0100

    support AsyncContext for PlayerFactory

--- freeroam-extended/server/freeroam-extended/Factories/PlayerFactory.cs
+++ freeroam-extended/server/freeroam-extended/Factories/PlayerFactory.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using AltV.Net;
+using AltV.Net.Async.CodeGen;
 using AltV.Net.Elements.Entities;
 
 namespace Freeroam_Extended.Factories
 {
-    public class AltPlayer : Player
+    public partial interface IAltPlayer : IPlayer
+    {
+        public IList<AltVehicle> Vehicles { get; set; }
+        public DateTime LastVehicleSpawn { get; set; }
+    } 
+    
+    [AsyncEntity(typeof(IAltPlayer))]
+    public partial class AltPlayer : Player, IAltPlayer
     {
         public IList<AltVehicle> Vehicles { get; set; }
         public DateTime LastVehicleSpawn { get; set; }
