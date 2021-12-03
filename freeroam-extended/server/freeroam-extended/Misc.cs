bf9b887ae5e4b445d085d6723a45a8d8c74f7d34
Tim <wuestenhagen.tim@gmail.com>
2021-12-03 19:43:53 +0100

    add more banned weapons + use hashset instead of list

--- freeroam-extended/server/freeroam-extended/Misc.cs
+++ freeroam-extended/server/freeroam-extended/Misc.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using AltV.Net.Data;
 
 namespace Freeroam_Extended
@@ -16,6 +17,20 @@ namespace Freeroam_Extended
             3125143736, // Pipe Bomb
             2874559379, // Proximity Mine
             741814745, // Sticky Bomb
+            1119849093, // Minigun
+            2138347493, // Firework
+            3056410471, // Widowmaker
+            615608432, // Molotov
+            883325847, // Jerry Can
+            2939590305, // Up n Atomizer
+            1198256469, // UnholyHellbringer    
+            4256991824, // Teargas
+            2694266206, // BZ-Gas
+            126349499, // Snowballs
+            101631238, // Fire Extinguisher
+            600439132, // Baseball
+            1233104067, // Flare
+            1198879012, // Flaregun
         };
 
         public static readonly IList<Position> SpawnPositions = new List<Position>
@@ -24,15 +39,15 @@ namespace Freeroam_Extended
             new(-1204.8923f, -3090.844f, 14.182373f), // Airport runway
             new(-1010.7165f, -2717.789f, 13.693726f) // Airport outside
         };
-        
-        public static IList<ulong> BannedPlayers = new List<ulong>
+
+        public static HashSet<Tuple<ulong, ulong>> BannedPlayers = new()
         {
-            // Add banned players here
+            
         };
         
-        public static IList<int> Operators = new List<int>
+        public static HashSet<int> Operators = new()
         {
-            // Add operators here
+            
         };
         
     }
