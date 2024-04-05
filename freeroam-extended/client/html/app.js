1c31ff6eafd985e251eb8e49a883cf8944c3ca57
Marcel N <67131061+xLuxy@users.noreply.github.com>
2024-04-05 21:41:54 +0200

    fix(chat-extended): Fix escapeString

--- freeroam-extended/client/html/app.js
+++ freeroam-extended/client/html/app.js
@@ -18,7 +18,7 @@ function escapeString(str) {
   if (typeof str !== "string") return str;
 
   return str
-    .replace(/&/g, "&amp;")
+    //.replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
     .replace(/"/g, "&quot;")
